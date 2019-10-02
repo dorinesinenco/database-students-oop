c34130df2d97fe8aa5b3e5ac80d3cb78be9d3900
Andrew <Andrew@test.hasdeu.md>
2019-10-02 22:00:04 +0300

    Lesson

---
+++ src/data/FakeTeacher.java
@@ -0,0 +1,5 @@
+package data;
+// For testing only
+public class FakeTeacher extends Teacher {
+
+}
--- src/data/SingleUniversityFactory.java
+++ src/data/SingleUniversityFactory.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import exceptions.OutOfRangeException;
 import helpers.Validator;
 
-public class University {
+public class SingleUniversityFactory {
 	String name;// - название университета ( валидация: min 3 .. max 30 )
 	private final int minNameLength = 3;
 	private final int maxNameLength = 30;
@@ -15,17 +15,53 @@ public class University {
 	ArrayList<Teacher> teachers;// - ссылка на список учителей университета
 	ArrayList<Student> students;// - ссылка на список студентов университета
 	
-	public University(String name, Administrator director, ArrayList<Group> groups, ArrayList<Domains> domains,
-			ArrayList<Teacher> teachers, ArrayList<Student> students) throws OutOfRangeException {
+	private static SingleUniversityFactory instance = new SingleUniversityFactory();
+	
+	public static SingleUniversityFactory getInstance() {
+		return instance;
+	}
+	
+
+	public PersonInterface getNewPerson(String type) {
+		if(type == null) {
+			return null;
+		}
+		if(shapeType.equalsIgnoreCase("Student")){
+	         return new Student();
+		}
+		if(shapeType.equalsIgnoreCase("Teacher")){
+	         return new Teacher();
+		}
+		if(shapeType.equalsIgnoreCase("FakeStudent")){
+	         return new Student();
+		}
+		if(shapeType.equalsIgnoreCase("FakeTeacher")){
+	         return new Teacher();
+		}
+		return null;
+	};
+	
+	public GroupInterface getNewGroup() {
+		if(type == null) {
+			return null;
+		}		
+		return new Group();
+	}
+	
+	
+	
+	private SingleUniversityFactory() throws OutOfRangeException {
 		super();
-		if(Validator.checkRange(name.length(), minNameLength, maxNameLength))
-			this.name = name;
-		else throw new OutOfRangeException("длинна название университета:"+name.length()+" ( валидация: min "+minNameLength+" .. max "+maxNameLength+" )");
-		this.director = director;
-		this.groups = groups;
-		this.domains = domains;
-		this.teachers = teachers;
-		this.students = students;
+//		
+//		if(Validator.checkRange(name.length(), minNameLength, maxNameLength))
+//			this.name = name;
+//		else throw new OutOfRangeException("длинна название университета:"+name.length()+" ( валидация: min "+minNameLength+" .. max "+maxNameLength+" )");
+//		this.director = director;
+		
+		this.groups = new ArrayList<Group>();
+		this.domains = new ArrayList<Domains>();
+		this.teachers = new ArrayList<Teacher>();
+		this.students = new ArrayList<Student>();
 	}
 	public String getName() {
 		return name;
